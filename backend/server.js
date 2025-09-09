fad53a55fedf9af5d0a5e25ac44d94857b49a035
“Hardatt” <hardatt7singhrathod@gmail.com>
2025-09-10 01:30:37 +0530

    Changes in some file

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("module-alias/register");
+const express = require("express");
+const app = express();
+const { sequelize } = require("@models");
+require("dotenv").config();
+
+app.use(express.json());
+app.use("/auth", require("@routes/authRoute"));
+app.use("/admin", require("@routes/adminRoute"));
+
+
+app.use((err,res,req,next)=>{
+  console.error(err.stack);
+  res.status(500).json({error: "Internal Server Error"});
+})
+sequelize.sync().then(() => {
+  console.log("Database connected...");
+  const PORT = process.env.PORT || 3005;
+  app.listen(PORT, (err) => {
+    if(err){
+      console.error("Server failed to start:",err);
+      process.exit(1);
+    }
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((error)=>{
+  console.error("Database connection failed:",error)
+  process.exit(1);
+});
